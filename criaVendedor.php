49f95e170bcd1acd56019413bf3e5f573242ce0f
Rian Imafuku <rianmelo100@gmail.com>
2021-04-22 00:28:32 -0300

    venda front,back

--- criaVendedor.php
+++ criaVendedor.php
@@ -9,6 +9,7 @@
 </head>
 <body class="scroll">
 <header>
+
 <h3 class="logout"><a href="php/logout.php">LOGOUT</a></h3>
 			<div class="caixa">
 				<h1><img height="220" class="logoempresa" src="img/logoempresa.png"></h1>
--- editaVenda.php
+++ editaVenda.php
@@ -7,18 +7,17 @@
 	<title>Editar venda</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 
-<body class="scroll">
+<body>
 	<header>
 	<h3 class="logout"><a href="php/logout.php">LOGOUT</a></h3>
 		<div class="caixa">
-			<h1><img height="220" class="logoempresa" src="img/logoempresa.png"></h1>
+			<h1><img height="250" class="logoempresa" src="img/logoempresa.png"></h1>
 
 			<nav>
 				<ul>
-					<li><a href="index.php">Início</a></li>
+					<li><a href="index.php">Home</a></li>
 					<li><a href="listaCliente.php">Clientes</a></li>
 					<li><a href="listaVendedor.php">Vendedores</a></li>
 					<li><a href="listaVeiculo.php">Veículos</a></li>
@@ -27,6 +26,7 @@
 			</nav>
 		</div>
 	</header>
+	<section class="grade">
 	<div class="container">
 		<form action="php/updateVenda.php" method="post">
 
@@ -37,31 +37,42 @@
 					<?php echo $_GET['Erro']; ?>
 				</div>
 			<?php } ?>
-			<div class="form-group">
-				<label for="nome">Nome</label>
-				<input type="text" class="form-control" id="nome" name="nome" value="<?= $row['nome'] ?>">
-			</div>
 
-			<div class="form-group">
-				<label for="cpf">CPF</label>
-				<input type="text" class="form-control" id="cpf" name="cpf" value="<?= $row['cpf'] ?>">
-			</div>
 
-			<div class="form-group">
-				<label for="telefone">Telefone</label>
-				<input type="text" class="form-control" id="telefone" name="telefone" value="<?= $row['telefone'] ?>">
-			</div>
+			<label>Vendedores</label>
+			<select name="id_ven">
+				<option disabled>Selecione</option>
+				<?php foreach ($Vendedores as $vendedores) : ?>
+					<option value="<?php echo $vendedores['id_ven']; ?>" <?php if ($vendedores['id_ven'] == $resultado['id_ven']) echo 'selected="selected"'; ?>><?php echo $vendedores['nome']; ?></option>
+				<?php endforeach; ?>
+			</select>
 
-			<div class="form-group">
-				<label for="endereco">Endereço</label>
-				<input type="text" class="form-control" id="endereco" name="endereco" value="<?= $row['endereco'] ?>">
+			<label>Clientes</label>
+			<select name="id_cli">
+				<option disabled>Selecione</option>
+				<?php foreach ($Clientes as $cliente) : ?>
+					<option value="<?php echo $cliente['id_cli']; ?>" <?php if ($cliente['id_cli'] == $resultado['id_cli']) echo 'selected="selected"'; ?>><?php echo $cliente['nomeCli']; ?></option>
+				<?php endforeach; ?>
+			</select>
+
+			<label>Veículos</label>
+			<select name="id_vei">
+				<option disabled>Selecione</option>
+				<?php foreach ($Veiculos as $veiculo) : ?>
+					<option value="<?php echo $veiculo['id_vei']; ?>" <?php if ($veiculo['id_vei'] == $resultado['id_vei']) echo 'selected="selected"'; ?>><?php echo $veiculo['modelo']; ?></option>
+				<?php endforeach; ?>
+			</select>
+
+			<div class="mb-3">
+				<label for="exampleFormControlTextarea1" class="form-label">Anotações: </label>
+				<textarea id="anotacoes" value="<?=$resultado['anotacoes'] ?>" class="form-control" id="anotacoes" rows="3"><?php echo $resultado['anotacoes']; ?></textarea>
 			</div>
-			<input type="text" name="id" value="<?= $row['id_ven'] ?>" hidden>
 
 			<button type="submit" class="btn btn-primary" name="update">Editar</button>
-			<a href="listaVendedor.php" class="link-primary">Visualizar</a>
+			<a href="listaVenda.php" class="retornar">Voltar</a>
 		</form>
 	</div>
+	</section>
 </body>
 
 </html>
